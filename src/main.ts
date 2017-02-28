21a8e61153de1f0e55da0cea8b3a1ebf71e23b66
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-02-28 14:45:18 +0100

    aot - enable prod mode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
-console.log('Running AOT compiled');
+
+enableProdMode();
+console.log('Running AOT compiled & ProdMode');
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
